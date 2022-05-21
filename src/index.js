bf6b4b593f3ebac970ce5020b0fcf4435ac53fdc
Sk Sagar <sagarsk205@gmail.com>
2022-05-21 23:22:26 +0530

    Class Component Created.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-    return <div> Hi There!</div>
-};
+
+class App extends React.Component{
+    render(){
+        //Geo Location Data.
+window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    err => console.log(err)
+);
+
+    return <div> Latitude: </div>
+}
+}
+
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.querySelector('#root')
 );
